02f5199512d3b2845c3b940301082b577c9862d6
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-31 21:18:50 +0530

    MyDetails.java

--- Lab_02/MyDetails/src/main/java/com/mycompany/mydetails/MyDetails.java
+++ Lab_02/MyDetails/src/main/java/com/mycompany/mydetails/MyDetails.java
@@ -5,7 +5,7 @@ public class MyDetails
 
     public static void main(String[] args)
     {
-        System.out.println("My name is Duleesha Jayangani");
+        System.out.println("My name is vinod rahal");
         System.out.println("Name of my degree program is Bsc(Hons)Software Engineering ");
     }
 }
