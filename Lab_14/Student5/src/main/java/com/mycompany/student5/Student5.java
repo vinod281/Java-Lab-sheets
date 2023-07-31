a45e32cb0b9d32955fb3ab09e2290f6755ff8950
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-31 21:25:06 +0530

    Add files via upload

---
+++ Lab_14/Student5/src/main/java/com/mycompany/student5/Student5.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.student5;
+
+public class Student5 
+{
+
+    
+     
+        
+    final int marks = 100;
+
+    final void display() 
+    {
+        System.out.println("Marks: " + marks);
+    }
+
+
+    }
---
+++ Lab_14/Student5/src/main/java/com/mycompany/student5/Undergraduate.java
@@ -0,0 +1,7 @@
+
+package com.mycompany.student5;
+
+public class Undergraduate
+{
+    
+}
