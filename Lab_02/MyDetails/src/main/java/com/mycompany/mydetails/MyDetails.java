c83ed3377c7efe0b5dcc6d9555f519624d176b52
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-31 21:14:10 +0530

    Add files via upload

---
+++ Lab_02/MyDetails/src/main/java/com/mycompany/mydetails/MyDetails.java
@@ -0,0 +1,11 @@
+package com.mycompany.mydetails;
+
+public class MyDetails
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Duleesha Jayangani");
+        System.out.println("Name of my degree program is Bsc(Hons)Software Engineering ");
+    }
+}
