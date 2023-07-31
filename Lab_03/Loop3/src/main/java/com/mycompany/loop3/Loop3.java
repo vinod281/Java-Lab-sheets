ab837655908f9186085de335b3d8df5b796b9f80
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-31 21:14:33 +0530

    Add files via upload

---
+++ Lab_03/Loop3/src/main/java/com/mycompany/loop3/Loop3.java
@@ -0,0 +1,13 @@
+package com.mycompany.loop3;
+
+public class Loop3
+{
+
+    public static void main(String[] args)
+    {
+        for(int a=0;a<5;a++)
+        {
+            System.out.println("Executing Loop " +a);
+        }
+    }
+}
